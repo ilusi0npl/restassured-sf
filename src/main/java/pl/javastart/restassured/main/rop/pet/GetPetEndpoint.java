33c1e5fe915fabbf42b5474232beb5a566b58fa7
ilusi0n <mateuszciolek@gmail.com>
2020-07-26 11:12:55 +0200

    Created resolution for task F12

---
+++ src/main/java/pl/javastart/restassured/main/rop/pet/GetPetEndpoint.java
@@ -0,0 +1,40 @@
+package pl.javastart.restassured.main.rop.pet;
+
+import io.qameta.allure.Step;
+import org.apache.http.HttpStatus;
+import pl.javastart.restassured.main.pojo.pet.Pet;
+import pl.javastart.restassured.main.request.configuration.RequestConfigurationBuilder;
+import pl.javastart.restassured.main.rop.BaseEndpoint;
+
+import java.lang.reflect.Type;
+
+import static io.restassured.RestAssured.given;
+
+public class GetPetEndpoint extends BaseEndpoint<GetPetEndpoint, Pet> {
+
+    private Integer petId;
+
+    @Override
+    protected Type getModelType() {
+        return Pet.class;
+    }
+
+    @Step("Get Pet")
+    @Override
+    public GetPetEndpoint sendRequest() {
+        response = given().spec(RequestConfigurationBuilder.getDefaultRequestSpecification())
+                .when().get("pet/{petId}", petId);
+        return this;
+    }
+
+    @Override
+    protected int getSuccessStatusCode() {
+        return HttpStatus.SC_OK;
+    }
+
+    public GetPetEndpoint setPetId(Integer petId) {
+        this.petId = petId;
+        return this;
+    }
+
+}
