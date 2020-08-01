e41002a2a1aabe94fb33512494b852c330e78267
ilusi0n <mateuszciolek@gmail.com>
2020-08-01 11:45:23 +0200

    Added resolution for task 15

---
+++ src/main/java/pl/javastart/restassured/main/rop/pet/FindPetByStatusEndpoint.java
@@ -0,0 +1,42 @@
+package pl.javastart.restassured.main.rop.pet;
+
+import io.qameta.allure.Step;
+import org.apache.http.HttpStatus;
+import pl.javastart.restassured.main.pojo.pet.Pet;
+import pl.javastart.restassured.main.request.configuration.RequestConfigurationBuilder;
+import pl.javastart.restassured.main.rop.BaseEndpoint;
+
+import java.lang.reflect.Type;
+
+import static io.restassured.RestAssured.given;
+
+public class FindPetByStatusEndpoint extends BaseEndpoint<FindPetByStatusEndpoint, Pet[]> {
+
+    private String status;
+
+    @Override
+    protected Type getModelType() {
+        return Pet[].class;
+    }
+
+    @Step("Get Pets by status")
+    @Override
+    public FindPetByStatusEndpoint sendRequest() {
+        response = given().spec(RequestConfigurationBuilder.getDefaultRequestSpecification())
+                .when()
+                .queryParam("status", status)
+                .get("pet/findByStatus");
+        return this;
+    }
+
+    @Override
+    protected int getSuccessStatusCode() {
+        return HttpStatus.SC_OK;
+    }
+
+    public FindPetByStatusEndpoint setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+}
